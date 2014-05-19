6c7f07daa7a7c9f31386824d044f3e631df674c7
Marcus Willock <crazcalm@gmail.com>
2014-05-19 15:46:10 -0400

    Finished notes on classes. Now I am about to start prototypes and inheritance

--- basics/javaScript/classes_prototypes_and_inheritance.js
+++ basics/javaScript/classes_prototypes_and_inheritance.js
@@ -28,3 +28,27 @@ function Shape () {
 var s = new Shape();
 s.move(10,10);
 console.log(s.distance_from_origin())
+
+// You can add as many properties and methods to your classes as you like, at
+// any time:
+
+var s = new Shape(15,35);
+s.FillColor = 'red';
+
+// The function that declares the class also serves as its constructor
+
+/*
+
+  There are two problems with this scheme for creating classes, however.
+
+  First, it seems a bit inefficient to have to carry around the method implementations
+with every single object. (Every time you create a new instance of Shape, you
+are creating the move and distance_from_origin functions from scratch)
+
+  Secondly, you might like to extend this class to create circles and squares
+and have them inherit the methods and properties from the base class without
+you having to do extra work.
+
+*/
+
+
