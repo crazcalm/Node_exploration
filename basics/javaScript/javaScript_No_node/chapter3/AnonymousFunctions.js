27ee394521a64f8799c9d250317a7ddf536ae278
Marcus Willock <crazcalm@gmail.com>
2014-06-26 17:35:20 -0400

    Callback function notes complete

--- basics/javaScript/javaScript_No_node/chapter3/AnonymousFunctions.js
+++ basics/javaScript/javaScript_No_node/chapter3/AnonymousFunctions.js
@@ -19,7 +19,7 @@ used without being assigned a name.
 */
 
 // Node says that there is a syntax error!
-//function(a){return a};
+//function(a){return a}
 
 /*
   Now, these anonymous pieces of data scattered around your code are not really
@@ -33,6 +33,67 @@ elegant uses for them:
     * You can define an anonymous function and execute it right away.
 
 Let's see these two applications of the anonymous functions in more detail.
+*/
+
+/*
+                    CallBack Functions
+                    ------------------
+
+  Because a function is just like any other data assinged to a variable, it can
+be defined, deleted, copied, and why not also passed as an argument to other
+functions?
+
+  Here's an example of a function that accepts two parameters, exceutes them,
+and returns the sum of what each of them returns.
+*/
+
+function invoke_and_add(a,b){
+  return a() + b();
+};
+
+/*
+  Now let's define two simple additional functions that only return hardcoded
+values.
+*/
+
+function one(){
+  return 1;
+}
+
+function two(){
+  return 2;
+}
+
+/*
+  Now we can pass those functions to the original function add() and get the
+result.
+*/
+
+console.log("invike_and_add(one, two): " +invoke_and_add(one, two))
+
+/*
+  Another example of passing a function as a parameter is to use anonymous
+functions. Instead of defining one() and two(), you can simple do:
+*/
+
+console.log("invoke_and_add with anonymous functions "+ 
+                invoke_and_add(function(){return 1;}, function(){return 2;}))
+
+/*
+  When you pass a function A to another function B and B executes A, it's often
+said that A is a callback function. If A doesn't have a name, then you can say
+that it's an anonymous callback function.
+
+  When are the callback functions useful? Let's see some examples that
+demonstrate the benefits of the callback functions, namely:
+
+    * They let you pass functions without the need to name them (which means
+      there are less global variables)
+
+    * You can delegate the responsibility of calling a function to another
+      function (which means there is less code to write)
+
+    * They can help with performance
 */
 
 
