d0e2c933a0f23b2f05b0972590cd5371a928bebd
Marcus Willock <crazcalm@gmail.com>
2014-07-17 14:35:40 -0400

    Notes on uber aka super

--- basics/javaScript/javaScript_No_node/Inheritance/AccessToTheParentFromAChildObject.js
+++ basics/javaScript/javaScript_No_node/Inheritance/AccessToTheParentFromAChildObject.js
@@ -10,3 +10,66 @@ same functionality. Let's rewrite the last example and, while taking care of
 inheritance, also create an uber property that points to the parent's
 prototype object.
 */
+
+function Shape(){}
+// augment prototype
+Shape.prototype.name = "shape"
+Shape.prototype.toString = function(){
+  var result = [];
+  if (this.constructor.uber){
+    result[result.length] = this.constructor.uber.toString();
+  }
+  result[result.length] = this.name;
+  return result.join(", ");
+};
+
+function TwoDShape(){}
+// take care of inheritance
+var F = function(){};
+F.prototype = Shape.prototype;
+TwoDShape.prototype = new F();
+TwoDShape.prototype.constructor = TwoDShape;
+TwoDShape.uber = Shape.prototype;
+// augment prototype
+TwoDShape.prototype.name = "2D shape";
+
+function Triangle (side, height){
+  this.side = side;
+  this.height = height;
+}
+
+// take care of inheritance
+var F = function(){};
+F.prototype = TwoDShape.prototype;
+Triangle.prototype = new F();
+Triangle.prototype.constructor = Triangle;
+Triangle.uber = TwoDShape.prototype;
+//augment prototype
+Triangle.prototype.name = "Triangle";
+Triangle.prototype.getArea = function(){return this.side * this.height /2;}
+
+
+/*
+  The new things here are:
+
+    * The way the uber property is set to point to the parent's prototype.
+    * The updated toString()
+
+  Previously, toString() only returned this.name. Now, in addition to that,
+there is a check to see whether this.constructor.uber exisits and, if it does,
+call its toString() first. this.constructor is the function itself, and
+this.constructor.uber points to the parent's prototype. The result is that
+when you call toString() for a Triangle instance, all toString() methods
+up the prototype chain are called.
+*/
+
+var my = new Triangle(5, 10);
+console.log(my.toString());
+
+/*
+  The name of the property uber could've been "superclass" but this would
+suggest that JavaScript has classes. Ideally it could've been "super"
+(as in Java), but "super" is a reserved word in JavaScript. The German word
+"uber" suggested by Douglass Crockford, means more or less the same as "super"
+and, you have to admit, is sounds uber-cool.
+*/
